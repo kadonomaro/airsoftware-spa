f179b5819351e9b4109845c24206b17249d484c1
kadonomaro <denis.afer@gmail.com>
2021-05-02 16:11:48 +0700

    init: add initial files

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        <h1>Vue</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {};
+        },
+    };
+</script>
